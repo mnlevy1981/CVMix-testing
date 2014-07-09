56c9dc286fa0747428a39ffd24ef00eadf14d313
Michael Levy <mike.levy.work@gmail.com>
2014-07-09 17:05:21 -0600

    Checks 8 builds (4 compilers, with/without netcdf)
    Refactored run_tests.sh to make it easy to change which modules are loaded,
    also finished adding settings for PGI and NAG compilers. At the very least, we
    can now automate this portion of the testing, though I want to add a little
    more detail (compiler versions, timestamp of last commit prior to testing, etc)
    to the output before I turn it into a cron job.
    
    Also, I added a time-stamp to the build directories -- this will ensure that
    every time I run the test it will create a clean checkout without having to
    wipe out the old checkouts (in case I want to investigate error messages).

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
+##############################################################################
+#                                                                            #
+#           CHANGE THESE ROUTINES IF MODULES NEED TO BE UPDATED              #
+#                                                                            #
+##############################################################################
+
+gnu_env ()
+{
+  module purge &>> $BLDLOG
+  module load compiler/gnu/4.4.7 &>> $BLDLOG
+}
+
+intel_env ()
+{
+  module purge &>> $BLDLOG
+  module load compiler/intel &>> $BLDLOG
+}
+
+pgi_env ()
+{
+      module purge &>> $BLDLOG
+      module load compiler/pgi &>> $BLDLOG
+}
+
+nag_env ()
+{
+      module purge &>> $BLDLOG
+      module load compiler/nag &>> $BLDLOG
+}
+
+##############################################################################
+#                                                                            #
+#                             MAIN ROUTINE                                   #
+#                                                                            #
+##############################################################################
+
 # 0) Initialize variables
-ROOTDIR=`pwd -P`
-#COMPILERS=( "gnu" "intel" "pgi" "nag" )
-COMPILERS=( "gnu" "intel" )
 DATE=`date +%y%m%d-%H%M%S`
-LOGFILE="$ROOTDIR/logs/log.goldbach.$DATE"
-FULLDUMP="$ROOTDIR/logs/FullOutput.goldbach.$DATE"
+ROOTDIR=`pwd -P`
+COMPILERS=( "gnu" "intel" "pgi" "nag" )
+SUMMARY_FILE="$ROOTDIR/logs/summary.$DATE"
+BLDLOG="$ROOTDIR/logs/buildlog.$DATE"
 ERR_CNT=0
 
 # 1) Check out 4 copies of repository (1 for each compiler)
@@ -14,23 +49,31 @@ REPO=git@github.com:CVMix/CVMix-src.git
 
 for compiler in ${COMPILERS[@]}
 do
-  TESTDIR=CVMix.$compiler
-  git clone $REPO $TESTDIR &>> $FULLDUMP
+  TESTDIR=CVMix-$compiler.$DATE
+  git clone $REPO $TESTDIR &>> $BLDLOG
   cd $TESTDIR
 
-  # SET ENVIRONMENT FOR BUILDING
+  # 1a) Set environment for building (load modules, set .CVMix_env)
   case $compiler in
     "gnu")
-      module purge &>> $FULLDUMP
-      module load compiler/gnu/4.4.7 &>> $FULLDUMP
-      echo "Building with GNU compiler!"
+      gnu_env
+      echo "Building with GNU compiler..."
       echo "FC = gfortran" > bld/.CVMix_env
     ;;
     "intel")
-      module purge &>> $FULLDUMP
-      module load compiler/intel &>> $FULLDUMP
-      echo "Building with intel compiler!"
+      intel_env
+      echo "Building with intel compiler..."
       echo "FC = ifort" > bld/.CVMix_env
+    ;;
+    "pgi")
+      pgi_env
+      echo "Building with PGI compiler..."
+      echo "FC = pgf90" > bld/.CVMix_env
+    ;;
+    "nag")
+      nag_env
+      echo "Building with NAG compiler..."
+      echo "FC = nagfor" > bld/.CVMix_env
     ;;
     *)
       echo "No option set for $compiler"
@@ -40,19 +83,24 @@ do
   echo "NETCDF_INC = $NETCDF_PATH/include" >> bld/.CVMix_env
   echo "NETCDF_LIB = $NETCDF_PATH/lib" >> bld/.CVMix_env
 
-# Build
+  # 1b) Build without netcdf
+  echo "Trying to build with all compilers:" >> $SUMMARY_FILE
   cd src
-  make &>> $FULLDUMP
-  if [ $? -ne 0 ]; then
-    echo "Non-netcdf $compiler Build failed" >> $LOGFILE
+  make &>> $BLDLOG
+  if [ $? -eq 0 ]; then
+    echo "Successfully built using $compiler without netcdf!" | tee -a $SUMMARY_FILE
+  else
+    echo "ERROR: Could not build using $compiler without netcdf!" | tee -a $SUMMARY_FILE
     ERR_CNT=$((ERR_CNT+1))
   fi
 
-  make netcdf &>> $FULLDUMP
-  if [ $? -ne 0 ]; then
-    echo "netcdf $compiler Build failed" >> $LOGFILE
+  # 1c) Build with netcdf
+  make netcdf &>> $BLDLOG
+  if [ $? -eq 0 ]; then
+    echo "Successfully built using $compiler with netcdf!" | tee -a $SUMMARY_FILE
+  else
+    echo "ERROR: Could not build using $compiler with netcdf!" | tee -a $SUMMARY_FILE
     ERR_CNT=$((ERR_CNT+1))
-    SKIP_COMPILER=TRUE
   fi
 
   # Back up to ROOTDIR for next compiler
